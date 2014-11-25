ba72d1dc29e175d3bf42492fb82576bf135fb785
Lizhou Wang <wlz1028@gmail.com>
2014-11-25 17:17:47 -0500

    description

--- backEnd/master.py
+++ backEnd/master.py
@@ -1,10 +1,15 @@
 from crawler import crawler
 from pagerank import page_rank
 from write_mongo import write_records
+from mongodb import drop_db
 
-def doc_id_index(links, inverted_doc_id):
+def doc_id_index(links, inverted_doc_id, desc):
     pr_results = page_rank(links)
-    mongo_doc = [{"_id": doc_id,"pageRank": link_id, "url": inverted_doc_id[doc_id]} for doc_id,link_id in pr_results.items()]
+    mongo_doc = [{"_id": doc_id,
+                "pageRank": link_id,
+                "url": inverted_doc_id[doc_id],
+                "title": desc[doc_id]['title'],
+                "description": desc[doc_id]['description']} for doc_id,link_id in pr_results.items()]
     write_records(mongo_doc, "csc326", "doc_id_index")
 
 def word_id_index(lexcon, inverted_index):
@@ -12,9 +17,10 @@ def word_id_index(lexcon, inverted_index):
     write_records(mongo_doc, "csc326", "word_id_index")
 
 def main(urls_file, _depth):
+    drop_db()
     bot = crawler(None, urls_file)
     bot.crawl(depth=_depth)
-    doc_id_index(bot.get_links(), bot.get_inverted_doc_id_cache())
+    doc_id_index(bot.get_links(), bot.get_inverted_doc_id_cache(), bot.get_url_description())
     word_id_index(bot.get_word_id(), bot.get_inverted_index())
 
 if __name__ == "__main__":
