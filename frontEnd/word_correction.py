ffbcaad34930baa0c8421569e81a5c83b4dba584
Lizhou Wang <wlz1028@gmail.com>
2014-11-29 18:06:11 -0500

    word cor

--- frontEnd/word_correction.py
+++ frontEnd/word_correction.py
@@ -2,17 +2,17 @@ import re, collections
 
 def words(text): return re.findall('[a-z]+', text.lower()) 
 
-def train(features):
+def train(from_dict):
     model = collections.defaultdict(lambda: 1)
-    for f in features:
+    for f in from_dict:
         model[f] += 1
     return model
 
-NWORDS = train(words(file('dictionary.txt').read()))
+WORDS_COUNT = train(words(file('dictionary.txt').read()))
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-def edits1(word):
+def check(word):
    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
    deletes    = [a + b[1:] for a, b in splits if b]
    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
@@ -20,14 +20,17 @@ def edits1(word):
    inserts    = [a + c + b     for a, b in splits for c in alphabet]
    return set(deletes + transposes + replaces + inserts)
 
-def known_edits2(word):
-    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+def check_again(word):
+    return set(e2 for e1 in check(word) for e2 in check(e1) if e2 in WORDS_COUNT)
 
-def known(words): return set(w for w in words if w in NWORDS)
+def known(words):
+    return set(w for w in words if w in WORDS_COUNT)
 
 def correct(word):
-    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-    return max(candidates, key=NWORDS.get)
+    print word
+    candidates = known([word]) or known(check(word)) or check_again(word) or [word]
+    #the word that appears more time in WORDS_COUNT
+    return max(candidates, key = WORDS_COUNT.get)
 
 def correction(keystring):
     keystring = keystring.strip()
@@ -37,4 +40,3 @@ def correction(keystring):
         correction.append(correct(word))
     corrected = " ".join(correction)
     return corrected
-
